d63d09e4378e16bc3580055d745499e2457d6fb9
Mario Antonio Alvarez Gómez <machimocho@gmail.com>
2020-09-23 19:11:00 -0500

    Models

---
+++ src/modelos/mensaje.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const { AppError } = require("../utils/errores")
+
+mongoose.set('useCreateIndex', true)
+
+const MensajeSchema = new mongoose.Schema({
+    username:{
+        type: mongoose.ObjectId,
+        required: true,
+        ref: 'Usuario'
+    },
+    message: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+}, {
+    timestamps: true
+})
+
+const Mensaje = mongoose.model('Mensaje', MensajeSchema)
+
+module.exports = Mensaje
